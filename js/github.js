e724a1cf348a9de82530d962b7b0a078a7d43339
dodois <dosvpn@protonmail.com>
2020-12-08 13:30:35 +0800

    Updates

--- js/github.js
+++ js/github.js
@@ -1,58 +1,94 @@
 const InitGithubInfo = {
-  BaseURL: "https://api.github.com/repos/dodois/dosvpn",
+  BaseURL:
+    'https://api.github.com/repos/dodois/dosvpn',
   get ReleaseListURL() {
-    return this.BaseURL + "/releases";
+    return this.BaseURL + '/releases';
   },
   get LatestReleaseURL() {
-    return this.ReleaseListURL + "/latest";
+    return this.ReleaseListURL + '/latest';
   },
 
   init() {
+    if (!location.href.includes('index.html'))
+      return; // 首页生效
     Promise.all([
       this.getRepo(),
       this.getLatestRelease(),
       this.getReleaseList(),
     ])
-      .then(([repo, latestRelease, releaseList]) => {
-        // this.processTitle(repo);
-        this.processStar(repo);
-        this.processDownload(latestRelease);
-        this.processDownloadNumber(releaseList);
-        // console.log(latestRelease.assets.filter(item => !!item.browser_download_url.match(/\.exe$/)));
-      })
+      .then(
+        ([repo, latestRelease, releaseList]) => {
+          // this.processTitle(repo);
+          this.processStar(repo);
+          this.processDownload(latestRelease);
+          this.processDownloadNumber(releaseList);
+          // console.log(latestRelease.assets.filter(item => !!item.browser_download_url.match(/\.exe$/)));
+        }
+      )
       .catch((err) => console.error(err));
   },
   getRepo() {
-    return fetch(this.BaseURL).then((res) => res.ok ? res.json() : Promise.reject(res.statusText));
+    return fetch(this.BaseURL).then((res) =>
+      res.ok
+        ? res.json()
+        : Promise.reject(res.statusText)
+    );
   },
   getLatestRelease() {
-    return fetch(this.LatestReleaseURL).then((res) => res.ok ? res.json() : Promise.reject(res.statusText));
+    return fetch(
+      this.LatestReleaseURL
+    ).then((res) =>
+      res.ok
+        ? res.json()
+        : Promise.reject(res.statusText)
+    );
   },
   getReleaseList() {
-    return fetch(this.ReleaseListURL).then((res) => res.ok ? res.json() : Promise.reject(res.statusText));
+    return fetch(
+      this.ReleaseListURL
+    ).then((res) =>
+      res.ok
+        ? res.json()
+        : Promise.reject(res.statusText)
+    );
   },
   processTitle(repo) {
     // console.log(repo, repo.description)
-    document.head.querySelector("title").innerText = `DOSVPN ${
-      repo.description || ""
+    document.head.querySelector(
+      'title'
+    ).innerText = `DOSVPN ${
+      repo.description || ''
     }`;
   },
   processStar(repo) {
     // stargazers_count
-    let starSpan = document.querySelector("#star-num");
-    starSpan.dataset.number = repo.stargazers_count || 0;
+    let starSpan = document.querySelector(
+      '#star-num'
+    );
+    starSpan.dataset.number =
+      repo.stargazers_count || 0;
   },
   processDownloadNumber(releaseList) {
     // let downloadFile = releaseList.assets.filter(item => !!item.browser_download_url.match(/\.exe$/))
-    let downloadSpan = document.querySelector("#download-num");
-    let totalDownload = releaseList.reduce((res, release) => {
-      res += release.assets.reduce((total, asset) => {
-        total += asset.download_count;
-        return total;
-      }, 0);
-      return res;
-    }, 0);
-    downloadSpan.dataset.number = (totalDownload / 1000).toFixed(1);
+    let downloadSpan = document.querySelector(
+      '#download-num'
+    );
+    let totalDownload = releaseList.reduce(
+      (res, release) => {
+        res += release.assets.reduce(
+          (total, asset) => {
+            total += asset.download_count;
+            return total;
+          },
+          0
+        );
+        return res;
+      },
+      0
+    );
+    downloadSpan.dataset.number = (
+      totalDownload / 1000
+    ).toFixed(1);
     // console.log('list => ', releaseList)
   },
   processDownload(latestRelease) {
@@ -60,18 +96,24 @@ const InitGithubInfo = {
     let downloadAssets =
       latestRelease.assets &&
       latestRelease.assets.filter(
-        (asset) => !!asset.browser_download_url.match(/\.exe$/)
+        (asset) =>
+          !!asset.browser_download_url.match(
+            /\.exe$/
+          )
       );
     // 如果各种原因导致资源不存在的默认资源
     downloadAssets =
       downloadAssets && downloadAssets.length
         ? downloadAssets
         : [
-            "https://github.com/dodois/dosvpn/releases/download/v1.1.2/Dos-VPN-Setup-1.1.2.exe",
+            'https://github.com/dodois/dosvpn/releases/download/v1.1.2/Dos-VPN-Setup-1.1.2.exe',
           ];
-    const downloadBtn = document.querySelector("#download-btn");
-    downloadBtn.target = "_blank";
-    downloadBtn.href = downloadAssets[0].browser_download_url;
+    const downloadBtn = document.querySelector(
+      '#download-btn'
+    );
+    downloadBtn.target = '_blank';
+    downloadBtn.href =
+      downloadAssets[0].browser_download_url;
   },
 };
 
